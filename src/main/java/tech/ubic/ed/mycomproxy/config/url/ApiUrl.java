1c8555edc6cbd8aa300d4e37b1ea6163d750290f
Vladimir Makarov <vmakarov@ubic.tech>
2021-07-02 11:42:25 +0300

    fix proxy and clean code

--- src/main/java/tech/ubic/ed/mycomproxy/config/url/ApiUrl.java
+++
@@ -1,8 +0,0 @@
-package tech.ubic.ed.mycomproxy.config.url;
-
-public final class ApiUrl {
-    public static final String PREFIX = "/api";
-    public static final String V1 = "/v1";
-    public static final String API = PREFIX + V1;
-    public static final String INTERNAL = "/internal";
-}
